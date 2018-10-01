c2d97f74ec10f01fa13d7357631b6737bc896270
Marc Longoria <marc.longoria@cbre.com>
2018-10-01 16:43:14 -0500

    commit nested events thumbnail component

--- Events/src/app/app.module.ts
+++ Events/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 
 import { EventsListComponent } from './events/events-list.component';
 import { EventsAppComponent } from './events-app.component';
+import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 
 @NgModule({
   declarations: [
-    EventsListComponent, EventsAppComponent
+    EventsListComponent, EventsAppComponent, EventsThumbnailComponent
   ],
   imports: [
     BrowserModule
--- Events/src/app/events-app.component.ts
+++ Events/src/app/events-app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'events-app',
   template: `
-    <nav-bar></nav-bar>
     <events-list></events-list>
   `
 })
