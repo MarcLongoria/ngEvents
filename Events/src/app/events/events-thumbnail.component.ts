1a47f64283c9b6e0f357ad44e5e37b1f4f5380be
Marc Longoria <marc.longoria@cbre.com>
2018-10-02 17:20:40 -0500

    commit events thumbnail plus more events

--- Events/src/app/events/events-thumbnail.component.ts
+++ Events/src/app/events/events-thumbnail.component.ts
@@ -2,18 +2,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'event-thumbnail',
-    template: `
-    <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
-    `
+    templateUrl: './events-thumbnail.component.html',
+    styleUrls: ['./events-thumbnail.component.css']
 })
 export class EventsThumbnailComponent {
 
