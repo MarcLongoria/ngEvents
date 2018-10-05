fec04ee938ad21ed34f56b9ac1fa8b50a8ff568a
Marc Longoria <marclongoria@outlook.com>
2018-10-05 16:43:25 -0500

    commit service

--- Events/src/app/app.module.ts
+++ Events/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsAppComponent } from './events-app.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { EventsThumbnailComponent } from './events/events-thumbnail.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
